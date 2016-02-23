42a51338f1e7ce901efbb7f38350e730b1411a5b
Alex Caruso <ajy256@wildcats.unh.edu>
2016-02-22 22:11:31 -0500

    Added activity to handle entering a new event.

--- app/src/main/java/edu/unh/cs/android/cackle/MainActivity.java
+++ app/src/main/java/edu/unh/cs/android/cackle/MainActivity.java
@@ -1,8 +1,9 @@
 package edu.unh.cs.android.cackle;
 
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -24,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
+        Intent intent = new Intent(MainActivity.this, NewEventActivity.class);
+        startActivity(intent);
       }
     });
 
