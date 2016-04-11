76f5b3f3f72814883a9f590f1b3c77e9e4149357
ChunkLightTuna <chris@oeleri.ch>
2016-04-11 16:29:56 -0400

    2 months

--- app/src/main/java/edu/unh/cs/android/cackle/Carcass.java
+++ app/src/main/java/edu/unh/cs/android/cackle/Carcass.java
@@ -1,8 +1,6 @@
 package edu.unh.cs.android.cackle;
 
 import android.graphics.Color;
-import android.os.Parcel;
-import android.os.Parcelable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,9 +8,9 @@ import java.util.List;
 /**
  * Created by Chris Oelerich on 2/22/16.
  */
-public class Carcass implements Parcelable {
+public class Carcass {
   int time;
-  int food_type;
+  int foodType;
   String building;
   String room;
   String victim;
@@ -22,8 +20,8 @@ public class Carcass implements Parcelable {
     return time;
   }
 
-  public int getFood_type() {
-    return food_type;
+  public int getFoodType() {
+    return foodType;
   }
 
   public String getBuilding() {
@@ -45,21 +43,12 @@ public class Carcass implements Parcelable {
   public Carcass() {} // Needed for Firebase to deserialize the data
 
   public Carcass(int food_type, int time, String building, String room, String victim, int difficulty) {
-    this.food_type = food_type;
+    this.foodType = food_type;
     this.time = time;
     this.building = building;
     this.room = room;
     this.victim = victim;
     this.difficulty = difficulty;
-  }
-
-  private Carcass(Parcel in) {
-    food_type = in.readInt();
-    time = in.readInt();
-    building = in.readString();
-    room = in.readString();
-    victim = in.readString();
-    difficulty = in.readInt();
   }
 
   public static List<Integer> availibleDifficultyLevels() {
@@ -70,7 +59,7 @@ public class Carcass implements Parcelable {
 
     int drawableId = 0;
 
-    switch (carcass.food_type) {
+    switch (carcass.foodType) {
       case Type.PIZZA:
         drawableId = R.drawable.pizza;
         break;
@@ -82,32 +71,5 @@ public class Carcass implements Parcelable {
   public class Type {
     public final static int PIZZA = 1;
     public final static int SUBS = 2;
-  }
-
-  public static final Parcelable.Creator<Carcass> CREATOR = new Parcelable.Creator<Carcass>() {
-    @Override
-    public Carcass createFromParcel(Parcel in) {
-      return new Carcass(in);
-    }
-
-    @Override
-    public Carcass[] newArray(int size) {
-      return new Carcass[size];
-    }
-  };
-
-  @Override
-  public void writeToParcel(Parcel out, int flags) {
-    out.writeInt(food_type);
-    out.writeInt(time);
-    out.writeString(building);
-    out.writeString(room);
-    out.writeString(victim);
-    out.writeInt(difficulty);
-  }
-
-  @Override
-  public int describeContents() {
-    return 0;
   }
 }
